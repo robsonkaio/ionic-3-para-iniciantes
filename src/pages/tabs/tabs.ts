092eebb3422f0bbffddacb9dcea2c0892975d091
Robson Kaio <robsonkaio@hotmail.com>
2017-12-02 08:11:41 -0200

    first commit

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { HomePage } from '../home/home';
+import { FeedPage } from '../feed/feed';
+import { LoginPage } from '../login/login';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  tab2Root = FeedPage;
+  tab3Root = LoginPage;
+  
+  constructor() {
+
+  }
+}
